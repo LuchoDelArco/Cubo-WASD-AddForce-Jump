3047f1b032372bfc8c08f8c6ead58b3b4d7ca825
LuchoDelArco <47138072@est.ort.edu.ar>
2022-08-29 12:02:11 -0300

    Save

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+	[SerializeField] GameObject dialogueUI;
+	[SerializeField] TextMeshProUGUI textoDelDialogo;
+	[SerializeField] bool hasTalked;
+	[SerializeField] string[] frasesDialogo;
+	[SerializeField] int posicionFrase;
+	[SerializeField] TextMeshProUGUI textoBoton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		dialogueUI.SetActive(false);
+	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Al entrar activa la UI de diálogo
+		if (other.gameObject.CompareTag("NPC"))
+		{
+			if (!hasTalked)
+			{
+				textoDelDialogo.text = "Hola forastero";
+				dialogueUI.SetActive(true);
+			}
+			else
+			{
+				textoDelDialogo.text = "ya hemos hablado";
+				dialogueUI.SetActive(true);
+				textoBoton.text = "Cerrar";
+			}
+		}
+
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("NPC"))
+		{
+			dialogueUI.SetActive(false);
+		}
+
+	}
+
+	public void NextFrase()
+	{
+		if (posicionFrase < frasesDialogo.Length)
+		{
+			textoDelDialogo.text = frasesDialogo[posicionFrase];
+
+			posicionFrase++;
+
+			if (posicionFrase == frasesDialogo.Length-1)
+			{
+				textoBoton.text = "Cerrar";
+			}
+		}
+		else
+		{
+			dialogueUI.SetActive(false);
+		}
+
+	}
+}
